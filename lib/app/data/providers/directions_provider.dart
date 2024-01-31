85f7ec011ea97bf92ff5de03647cf62cdac8d6fe
Firdaus Alam Hudi <alanfirdaus999@gmail.com>
2024-01-31 16:17:56 +0700

    style: url router directions

--- lib/app/data/providers/directions_provider.dart
+++ lib/app/data/providers/directions_provider.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 class DirectionsProvider {
   Future<dynamic> getDirections(data) async {
     var url = Uri.parse(
-        'http://router.project-osrm.org/route/v1/driving/$data?steps=true&annotations=true&geometries=geojson&overview=full');
+        'https://router.project-osrm.org/route/v1/driving/$data?steps=true&annotations=true&geometries=geojson&overview=full');
     var response = await http.get(url);
 
     return response;
