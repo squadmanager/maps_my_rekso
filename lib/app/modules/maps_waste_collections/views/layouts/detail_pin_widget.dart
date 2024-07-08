077cb1377cbb88df404cf7ddc89684e13b545678
Firdaus Alam Hudi <alanfirdaus999@gmail.com>
2024-07-08 16:21:09 +0700

    feat: change carousel slider

--- lib/app/modules/maps_waste_collections/views/layouts/detail_pin_widget.dart
+++ lib/app/modules/maps_waste_collections/views/layouts/detail_pin_widget.dart
@@ -1,5 +1,5 @@
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_carousel_widget/flutter_carousel_widget.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -16,12 +16,13 @@ class DetailPinWidget extends GetView<MapsWasteCollectionsController> {
   const DetailPinWidget({required this.element, super.key});
 
   Widget listImage(context) {
-    return CarouselSlider(
+    return FlutterCarousel(
       options: CarouselOptions(
+        height: Get.height,
         autoPlay: true,
         aspectRatio: 50,
         enlargeCenterPage: true,
-        enlargeStrategy: CenterPageEnlargeStrategy.zoom,
+        enlargeStrategy: CenterPageEnlargeStrategy.scale,
       ),
       items: [
         for (int i = 0; i < controller.attachmentList.length; i++)
@@ -115,6 +116,105 @@ class DetailPinWidget extends GetView<MapsWasteCollectionsController> {
           ),
       ],
     );
+    // return CarouselSlider(
+    //   options: CarouselOptions(
+    //     autoPlay: true,
+    //     aspectRatio: 50,
+    //     enlargeCenterPage: true,
+    //     enlargeStrategy: CenterPageEnlargeStrategy.zoom,
+    //   ),
+    //   items: [
+    //     for (int i = 0; i < controller.attachmentList.length; i++)
+    //       Container(
+    //         margin: const EdgeInsets.all(5.0),
+    //         child: ClipRRect(
+    //           borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+    //           child: InkWell(
+    //             onTap: () => Navigator.push(
+    //               context,
+    //               MaterialPageRoute(
+    //                 builder: (context) => PhotoViewWidget(
+    //                   image: controller.attachmentList[i].url,
+    //                 ),
+    //               ),
+    //             ),
+    //             child: Stack(
+    //               children: [
+    //                 Image.network(
+    //                   controller.attachmentList[i].url,
+    //                   fit: BoxFit.cover,
+    //                   width: 1000.0,
+    //                   loadingBuilder: (BuildContext context, Widget child,
+    //                       ImageChunkEvent? loadingProgress) {
+    //                     if (loadingProgress == null) {
+    //                       return child;
+    //                     }
+    //                     return Center(
+    //                       child: CircularProgressIndicator(
+    //                         value: loadingProgress.expectedTotalBytes != null
+    //                             ? loadingProgress.cumulativeBytesLoaded /
+    //                                 loadingProgress.expectedTotalBytes!
+    //                             : null,
+    //                       ),
+    //                     );
+    //                   },
+    //                 ),
+    //                 Positioned(
+    //                   bottom: 0.0,
+    //                   left: 0.0,
+    //                   right: 0.0,
+    //                   child: Container(
+    //                     decoration: const BoxDecoration(
+    //                       gradient: LinearGradient(
+    //                         colors: [
+    //                           Color.fromARGB(200, 0, 0, 0),
+    //                           Color.fromARGB(0, 0, 0, 0)
+    //                         ],
+    //                         begin: Alignment.bottomCenter,
+    //                         end: Alignment.topCenter,
+    //                       ),
+    //                     ),
+    //                     padding: const EdgeInsets.symmetric(
+    //                         vertical: 10.0, horizontal: 20.0),
+    //                     child: Column(
+    //                       crossAxisAlignment: CrossAxisAlignment.start,
+    //                       children: [
+    //                         Text(
+    //                           'Image ${i + 1}',
+    //                           style: GoogleFonts.poppins(
+    //                             fontSize: 20.0,
+    //                             fontWeight: FontWeight.bold,
+    //                             color: HexColor(
+    //                                 ColorWidget().primaryWasteCollections),
+    //                           ),
+    //                         ),
+    //                         Text(
+    //                           controller.attachmentList[i].createdAt != null ||
+    //                                   controller.attachmentList[i].createdAt !=
+    //                                       ''
+    //                               ? 'Created : ${DateFormat("d MMMM y hh:mm", "id_ID").format(
+    //                                   DateTime.parse(controller
+    //                                       .attachmentList[i].createdAt),
+    //                                 )}'
+    //                               : 'Created : -',
+    //                           style: GoogleFonts.poppins(
+    //                             fontSize: 12.0,
+    //                             fontWeight: FontWeight.normal,
+    //                             color: HexColor(
+    //                                 ColorWidget().primaryWasteCollections),
+    //                           ),
+    //                         ),
+    //                       ],
+    //                     ),
+    //                   ),
+    //                 ),
+    //               ],
+    //             ),
+    //           ),
+    //         ),
+    //       ),
+    //   ],
+    // );
   }
 
   @override
