6e9ea18acf77c7be3386aff17a85ff5a824c383e
Firdaus Alam Hudi <alanfirdaus999@gmail.com>
2024-07-11 11:46:20 +0700

    fix: bug

--- lib/app/modules/maps_street_cleaning/views/layouts/lay_card_mc_vehicle_directions_sc.dart
+++ lib/app/modules/maps_street_cleaning/views/layouts/lay_card_mc_vehicle_directions_sc.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -140,36 +139,32 @@ class McVehicleDirectionsMinimize
 
   @override
   Widget build(BuildContext context) {
-    return Animate(
-      effects: const [FadeEffect(), ScaleEffect()],
-      delay: 500.ms,
-      child: Material(
-        borderRadius: BorderRadius.circular(8.0),
-        color: HexColor(ColorWidget().primary),
-        child: InkWell(
-          onTap: () => controller.mcMinimizeTripData(false),
-          child: Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SvgPicture.asset(
-                  'assets/icons/expand-arrows-alt.svg',
+    return Material(
+      borderRadius: BorderRadius.circular(8.0),
+      color: HexColor(ColorWidget().primary),
+      child: InkWell(
+        onTap: () => controller.mcMinimizeTripData(false),
+        child: Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                'assets/icons/expand-arrows-alt.svg',
+                color: HexColor(ColorWidget().white),
+                height: 15,
+                width: 15,
+              ),
+              const SizedBox(width: 5.0),
+              Text(
+                'Show Detail Trip',
+                style: GoogleFonts.poppins(
+                  fontSize: 12.0,
+                  fontWeight: FontWeight.w500,
                   color: HexColor(ColorWidget().white),
-                  height: 15,
-                  width: 15,
                 ),
-                const SizedBox(width: 5.0),
-                Text(
-                  'Show Detail Trip',
-                  style: GoogleFonts.poppins(
-                    fontSize: 12.0,
-                    fontWeight: FontWeight.w500,
-                    color: HexColor(ColorWidget().white),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
